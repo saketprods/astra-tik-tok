3528cf039220ca8fc092577102f3d19bf611152d
root <TikTok@example.com>
2021-04-22 07:50:19 +0000

    Step 5 final

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import Header from './components/Header'
+import { HashRouter, Route , Switch} from 'react-router-dom'
+import Home from './pages/Home'
+import Upload from './pages/Upload'
 import './App.css'
+
 const App = () => {
   return (
-    <div className="App">
-    </div>
+    <HashRouter>
+       <Header />
+    <Switch>
+      <Route path= "/upload" component={Upload}/>
+      <Route path= "/" component={Home}/>
+    </Switch>
+  </HashRouter>
   );
 }
+
 export default App
